5c857b158f3dd75c39d81980d3175dfe30533dec
donxnahte <donxnnn@gmail.com>
2026-02-11 09:17:28 +0300

    first commit

---
+++ python_basics/challenge1.py
@@ -0,0 +1,7 @@
+uni = "JKUAT"
+grade = "A-"
+course = "Computer Science"
+cluster_points = 69
+print(f"My favourite university is {uni}")
+print(f"My preferred course is {course}")
+print(f"My cluster points are {cluster_points}")
---
+++ python_basics/hello.py
@@ -0,0 +1,12 @@
+#This is a comment
+print("Hello world, don here👋🏾")
+#city
+print("I'm from Mombasa")
+#My hobbies
+print("I like to binge shows, watch movies, video games, music and computers")
+age = 21
+height = 2.1
+weight = 69
+print(f"I'm {age} years old")
+print(f"I weigh {weight} kilograms")
+print(f"I'm also {height} metres tall")
