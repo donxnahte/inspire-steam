7f212bf977600560dc75521deccab351a9a2bc83
donxnahte <donxnnn@gmail.com>
2026-02-11 10:39:25 +0300

    input

---
+++ python_basics/numbers.py
@@ -0,0 +1,14 @@
+#Name :Ethan Mbila
+#Date :11/02/2026
+#Program to add, subtract, divide, multiply two numbers from imput
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1+num2
+difference = num1-num2
+quotient = num1/num2
+product = num1*num2
+print(f"The sum is {sum}")
+print(f"The difference is {difference}")
+print(f"The quotient is {quotient}")
+print(f"The product is {product}")
---
+++ python_basics/user_input.py
@@ -0,0 +1,5 @@
+#Name :Ethan Mbila
+#Date :11/02/2026
+#Program to get user input
+name = input("what's yo name? ")
+print(name)
