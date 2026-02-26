a14b142f152cb86ec9fd94f733438a06478dfd2f
donxnahte <donxnnn@gmail.com>
2026-02-26 10:45:19 +0300

    IoT using Wokwi

---
+++ IoT/leds.py
@@ -0,0 +1,16 @@
+#Name :Ethan Mbila
+#Date :26/02/2026
+
+from machine import Pin
+import time
+
+red_led = Pin(28, Pin.OUT)
+yellow_led = Pin(27, Pin.OUT)
+while True:
+  red_led.on()
+  yellow_led.off()
+  time.sleep(1) # Wait for USB to become ready
+  red_led.off()
+  yellow_led.on()
+  time.sleep(1)
+  print("Learning IoT")
